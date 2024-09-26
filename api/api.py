54adec0343c209e60f54e481c460bdf3c32eded9
Mengjiao Han <mhan@pn2306614.lanl.gov>
2024-09-26 14:27:42 -0600

    first commit

--- api/api.py
+++ api/api.py
@@ -1,8 +1,22 @@
-import time
+# import time
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/time')
+# def get_current_time():
+#     print(time.time())
+#     return '{"time": time.time()}'
+
 from flask import Flask
 
-app = Flask(__name__)
+api = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@api.route('/profile')
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
